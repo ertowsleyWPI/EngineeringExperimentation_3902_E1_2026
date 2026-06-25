d90c679c31e07b8823c5866bf14edc4af5032a83
ertowsleyWPI <ertowsley@wpi.edu>
2026-06-25 08:51:41 -0400

    Update Test_3.ino
    First stab at including the OLED and US sensors. Almost certainly going to require debugging

--- Test_3/Test_3.ino
+++ Test_3/Test_3.ino
@@ -11,18 +11,49 @@
 
 
   Update 1: Got the ToF Sensor functioning, had to solder the board
-
+  Update 2: Got the ToF Sensor dialed in 
 
 */
 
-
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Adafruit_VL53L0X.h>
+
+//OLED SETTINGS
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+//Global Variables 
+
+const int trigPin = 4; //Ultrasonic Pulse Trigger Pin
+const int echoPin = 5; //Ultrasonic Pulse Echo Pin
+
+float distanceUS, distanceToF, duration;
 
 //Create an object of the Adafruit_VL53L0X class
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println("SSD1306 Allocation Failed");
+    while (true);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.display();
 
   // wait until serial port opens for native USB devices
   while (!Serial) {
@@ -47,6 +78,23 @@ void setup() {
 
 
 void loop() {
+  
+
+  //US Related
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); // Short to allow sensor stabilization
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); // Pulse Duration
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH); // Read the duration of the echo pulse (in microseconds)
+
+  distanceUS = (duration * 0.0343) / 2;
+
+  //ToF Sensor Related
+  
   VL53L0X_RangingMeasurementData_t measure;
 
   Serial.print(millis());
@@ -55,11 +103,30 @@ void loop() {
 
   if (measure.RangeStatus != 4) {  // phase failures have incorrect data
     Serial.print("Distance (mm): ");
-    int distanceToV = measure.RangeMilliMeter;
-    Serial.println(distanceToV);
+    distanceToF = measure.RangeMilliMeter;
+    Serial.println(distanceToF);
   } else {
     Serial.println(" out of range ");
   }
+    Serial.print(" , ");
+    Serial.println(distanceUS);
 
+  delay(1000); // to make debugging easier
+
+  //OLED Output
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.setCursor(0, 20);
+  display.println("ToF Distance: ");
+  display.print(distanceToF);
+  display.println(" mm");
+  display.println("US Distance: ");
+  display.print(distanceUS);
+  display.println(" cm");
+
+  display.display();
   delay(100);
+
 }
