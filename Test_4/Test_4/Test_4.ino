69294c1f2aa155fc7f0c5a7b17c352331f050353
ertowsleyWPI <ertowsley@wpi.edu>
2026-08-19 10:13:52 -0400

    Update Test_4.ino
    Started mostly from scratch, got the stepper working with a slightly underpowered power supply and a hack job to make the port work with a soldering iron.
    
    Close to getting the Hall sensor to work however I cant get it to trip with my magnet

--- Test_4/Test_4/Test_4.ino
+++ Test_4/Test_4/Test_4.ino
@@ -1,142 +1,52 @@
-/*
-  This program tests the Adafruit VL53L0X Time of Flight Distance Sensor.
-  It initializes the sensor, reads a distance measurement, and prints the 
-  distance in millimeters to the serial monitor.
-  Note: The VL53L0X can handle about 50 - 1200 mm of range distance.
+#include <Stepper.h>
 
-  Board: (Originally wrote for a Arduino Uno R4 (or R3), changed to a Nano for the remainder of this experiment)
-  Component: Time of Flight Distance Sensor (GY-530 base on VL53L0X)
-  Library: https://github.com/adafruit/Adafruit_VL53L0X  (Adafruit_VL53L0X by Adafruit)
+//1.8 deg per step -> 200 steps per Rev
 
 
+const int StepsPerRev = 200;
+int sensor2 = 3; // sensor pin
+int val2; // 1: Magnetic field detected, 0: No magnetic field detected
 
-  Update 1: Got the ToF Sensor functioning, had to solder the board
-  Update 2: Got the ToF Sensor dialed in 
 
-*/
-
-#include <Wire.h>
-#include <SSD1306Ascii.h>
-#include <SSD1306AsciiWire.h>
-#include <Adafruit_VL53L0X.h>
-
-//OLED SETTINGS
-
-#define I2C_ADDRESS 0x3C
-
-SSD1306AsciiWire display;
-
-//Global Variables 
-
-const int trigPin = 4; //Ultrasonic Pulse Trigger Pin
-const int echoPin = 5; //Ultrasonic Pulse Echo Pin
-const int irPin = 8; // IR Sensor Pin
-
-int irState; // Indicates the sensor being triggered or not
-
-float distanceUS, distanceToF, duration, distCorrect;
-
-//Create an object of the Adafruit_VL53L0X class
-Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+//Stepper Library
+// (Steps, AIN1, AIN2, BIN1, BIN2)
+Stepper nema17Stepper(StepsPerRev, 5, 6, 4, 7);
 
 void setup() {
-  Serial.begin(115200);
+  //Settup code, runs once
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(irPin, INPUT);
+  pinMode(sensor2, INPUT); // set sensor pin as input
 
-  Wire.begin();
-  display.begin(&Adafruit128x64, I2C_ADDRESS);
-  display.setFont(Adafruit5x7);
-  display.clear();
-  display.println("Initial Boot");
+  //Motor Speed (rpm)
 
-  // wait until serial port opens for native USB devices
-  while (!Serial) {
-    delay(1);
-  }
+  nema17Stepper.setSpeed(10);
 
-  //Initialize the sensor
-  Serial.println("Adafruit VL53L0X test");
-  if (!lox.begin()) {
-    Serial.println(F("Failed to boot VL53L0X"));
-    while (1)  //Stop the program if the sensor cannot be initialized
-      ;
-  }
+  Serial.begin(9600);
 
-  Serial.println(F("VL53L0X API Simple Ranging example\n\n"));
+  Serial.println("NEMA 17 is initialized with TB6612");
 
-  Serial.print(F("Time in Milliseconds"));
-  Serial.print(F(" , "));
-  Serial.println(F("Distance"));
 
 }
 
-
 void loop() {
-  
 
-  //US Related
+  //main code loops
 
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2); // Short to allow sensor stabilization
+  Serial.println("Moving Clockwise");
+  val2 = digitalRead(sensor2); // Read the sensor
+  Serial.print("ky003 value: ");
+  Serial.println(val2);
+  nema17Stepper.step(StepsPerRev);
 
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10); // Pulse Duration
-  digitalWrite(trigPin, LOW);
+  delay(1000);
 
-  duration = pulseIn(echoPin, HIGH); // Read the duration of the echo pulse (in microseconds)
+  Serial.println("Moving Counter Clockwise");
+  val2 = digitalRead(sensor2); // Read the sensor
+  Serial.print("ky003 value: ");
+  Serial.println(val2);
+  nema17Stepper.step(-StepsPerRev);
 
-  distanceUS = 10 * (duration * 0.0343) / 2;
+  delay(1000);
 
-  irState = digitalRead(irPin);
-
-  distCorrect = 11.55; // This is a magic number taken from the CoolTerm work, where the average difference was 11.55mm. Once smoothed byt this ammount, the results were shockily close to each other.0
-
-  //ToF Sensor Related
-  
-  VL53L0X_RangingMeasurementData_t measure;
-
-  Serial.print(millis());
-  Serial.print(" , ");
-  lox.rangingTest(&measure, false);  // pass in 'true' to get debug data printout!
-
-  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    Serial.print(F("Distance (mm): "));
-    // distanceToF = measure.RangeMilliMeter - distCorrect; Tried initially but I believe it messed with some of the internal calibrations to these sensors
-    distanceToF = measure.RangeMilliMeter;
-    Serial.println(distanceToF);
-  } else {
-    Serial.println(F(" out of range "));
-    distanceToF = 0 ; // This should unlock the hung stage of the TOF sensor when the object is removed. If it shows 0 it simply does not detect and object at that time.
-  }
-    Serial.print(F(" , "));
-    Serial.println(distanceUS);
-
-  delay(5); // to make debugging easier
-
-  //OLED Output
-
-  display.clear(); // Clears the screen directly
-  display.setCursor(0, 0); // Back to top left
-  
-  display.println("ToF Distance: ");
-  display.print(distanceToF);
-  display.println(" mm");
-  
-  display.println("US Distance: ");
-  display.print(distanceUS);
-  display.println(" mm");
-  
-  display.println("IR Status: ");
-    if (irState == LOW){
-      display.println("Object Detected"); // Low behavior is a returned signal
-    } else {
-        display.println("Clear");
-    }
-
-
-  // Removed display.display();
 
 }
