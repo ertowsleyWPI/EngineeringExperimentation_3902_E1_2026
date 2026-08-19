11c75dc48d8698eda19ad9dd04ebeda5a695257a
ertowsleyWPI <ertowsley@wpi.edu>
2026-08-19 10:48:29 -0400

    Update Test_4.ino
    Got sidetracked by a fubar hall sensor. Luckily I had a multipack and swapping it fixed the problem

--- Test_4/Test_4/Test_4.ino
+++ Test_4/Test_4/Test_4.ino
@@ -4,8 +4,9 @@
 
 
 const int StepsPerRev = 200;
-int sensor2 = 3; // sensor pin
+int sensor2 = 10; // sensor pin
 int val2; // 1: Magnetic field detected, 0: No magnetic field detected
+int lastVal2 = 1;
 
 
 //Stepper Library
@@ -15,7 +16,7 @@ Stepper nema17Stepper(StepsPerRev, 5, 6, 4, 7);
 void setup() {
   //Settup code, runs once
 
-  pinMode(sensor2, INPUT); // set sensor pin as input
+  pinMode(sensor2, INPUT_PULLUP); // set sensor pin as input
 
   //Motor Speed (rpm)
 
@@ -33,20 +34,36 @@ void loop() {
   //main code loops
 
   Serial.println("Moving Clockwise");
-  val2 = digitalRead(sensor2); // Read the sensor
-  Serial.print("ky003 value: ");
-  Serial.println(val2);
-  nema17Stepper.step(StepsPerRev);
-
+  for(int i = 0; i < StepsPerRev; i++) {
+    nema17Stepper.step(1); 
+    checkSensor();
+  }
   delay(1000);
 
   Serial.println("Moving Counter Clockwise");
-  val2 = digitalRead(sensor2); // Read the sensor
-  Serial.print("ky003 value: ");
-  Serial.println(val2);
-  nema17Stepper.step(-StepsPerRev);
+  for(int i = 0; i < StepsPerRev; i++) {
+    nema17Stepper.step(-1); 
+    checkSensor();
+  }
 
   delay(1000);
 
 
+
+
+}
+
+
+void checkSensor() {
+  val2 = digitalRead(sensor2);
+  
+  // Only print if the sensor state has CHANGED (prevents massive text spam)
+  if (val2 != lastVal2) {
+    if (val2 == 0) {
+      Serial.println("Magnet is detected!");
+    } else {
+      Serial.println("Magnet is not detected.");
+    }
+    lastVal2 = val2; // Update the state
+  }
 }
